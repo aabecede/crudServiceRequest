cc9945df96a42c07e17cd619689e755fcb1d5658
abuduru <aabdul.jabbar13@gmail.com>
2020-08-11 01:05:10 +0700

    [templating] - basic template

---
+++ resources/views/layout/content.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+@section('content')
+<div class="row">
+    <div class="col-12">
+        <h4>bukan dsni crudnya pilih pojok kanan atas</h4>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,8 @@
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <ul class="nav nav-pills">
+        <li class="nav-item">
+            <a href="{{route('alamat.index')}}">Coba Crud</a>
+        </li>
+    </ul>
+</nav>
